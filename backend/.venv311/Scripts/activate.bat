f5103037e012de9fa78c1e916e885ab5936f132b
WingedKing2311 <marcusabrahamt@gmail.com>
2026-09-25 21:58:34 +0530

    Admin panel filtering 2

--- backend/.venv311/Scripts/activate.bat
+++ backend/.venv311/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91877\Desktop\Canteen-systemworking\backend\.venv311
+set VIRTUAL_ENV=C:\Users\91877\Desktop\Projects\Canteen-systemworking\backend\.venv311
 
 if not defined PROMPT set PROMPT=$P$G
 
